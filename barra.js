1ad39e660430088844eb16ba294a47ab33b2ecaa
RAYUDONUTS <143645509+RAYUDONUTS@users.noreply.github.com>
2024-04-11 13:19:32 -0500

    Add files via upload

---
+++ barra.js
@@ -0,0 +1,13 @@
+var lastScrollTop = 0;
+
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var menuButton = document.getElementById('menu');
+
+    if (scrollPosition > lastScrollTop && scrollPosition > 80) {
+        menuButton.style.top = '90px'; // Mover hacia abajo solo si el desplazamiento es hacia abajo y más de 80px
+    } else {
+        menuButton.style.top = '10px'; // Volver a la posición original si el desplazamiento es hacia arriba
+    }
+    lastScrollTop = scrollPosition;
+});
