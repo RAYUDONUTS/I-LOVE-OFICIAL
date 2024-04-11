e887f29ff6108633426b79b99f46c0d313154e96
RAYUDONUTS <143645509+RAYUDONUTS@users.noreply.github.com>
2024-04-11 13:30:47 -0500

    Update barra.js
    70px

--- barra.js
+++ barra.js
@@ -5,7 +5,7 @@ window.addEventListener('scroll', function() {
     var menuButton = document.getElementById('menu');
 
     if (scrollPosition > lastScrollTop && scrollPosition > 80) {
-        menuButton.style.top = '90px'; // Mover hacia abajo solo si el desplazamiento es hacia abajo y más de 80px
+        menuButton.style.top = '70px'; // Mover hacia abajo solo si el desplazamiento es hacia abajo y más de 80px
     } else {
         menuButton.style.top = '10px'; // Volver a la posición original si el desplazamiento es hacia arriba
     }
